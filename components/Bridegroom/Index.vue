3648099c8f81b4ca07fabb25e4a42a4d3dc64b55
riokusuma70 <90307282+riokusuma70@users.noreply.github.com>
2023-08-15 01:02:49 +0800

    Update Index.vue

--- components/Bridegroom/Index.vue
+++ components/Bridegroom/Index.vue
@@ -4,7 +4,7 @@
 			<h2 class="font-body font-serif text-5xl text-yellow-800 dark:text-yellow-300/90 md:text-7xl">Pengantin</h2>
 			<h3 class="mx-auto font-serif mt-5 max-w-xs text-sm font-semibold text-slate-600 dark:text-black-100 md:max-w-full">Allah SWT telah mengatur kehidupan makhluknya berpasang-pasangan. maka dari itu perkenankanlah kami mempersatukan putra-putri kami:</h3>
 		</div>
-		<div class="mx-auto font-serif dark:text-black-100 flex max-w-2xl flex-col items-center justify-evenly gap-y-10 md:flex-row md:gap-y-0 lg:max-w-full">
+		<div class="mx-auto font-serif dark:text-brown-200 flex max-w-2xl flex-col items-center justify-evenly gap-y-10 md:flex-row md:gap-y-0 lg:max-w-full">
 			<BridegroomProfile name="Briptu Ivan Andhika Dani Anwar, S.H." instagramURL="https://www.instagram.com/ivanandhika/">
 				<template #family>Putra pertama dari Bapak M. Anwar B Rusmana & Ibu Umi Sunariyati</template>
 				<template #image>
@@ -21,7 +21,7 @@
 					<FontAwesomeIcon :icon="['fab', icon]" size="lg" inverse />
 				</template>
 			</BridegroomProfile>
-			<span class="text-slate:600 font-serif font-body text-5xl dark:text-black-100 md:-mt-52 md:text-7xl" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150">&</span>
+			<span class="text-slate:600 font-serif font-body text-5xl dark:text-brown-200 md:-mt-52 md:text-7xl" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="150">&</span>
 			<BridegroomProfile name="Virginia Khofifah Akhmad" instagramURL="https://www.instagram.com/virgin.akhmad/">
 				<template #family>Putri pertama dari Bapak Akhmad Dimyanti & Ibu Eni Susanti</template>
 				<template #image>
