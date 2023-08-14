f6c5612503d91439517f0c2d16a3d45421a9d87b
riokusuma70 <90307282+riokusuma70@users.noreply.github.com>
2023-08-15 01:18:46 +0800

    Update Verse.vue

--- components/Verse.vue
+++ components/Verse.vue
@@ -1,8 +1,8 @@
 <template>
-	<section class="bg-stone-400 pt-10 pb-10 dark:bg-stone-200 md:pt-10 md:pb-20">
+	<section class="bg-stone-400 py-6 text-center dark:bg-stone-200  rounded-full border-4 border-yellow-800 object-cover  dark:border-yellow-300/90 ">
 		<figure class="mx-auto w-3/4" data-aos="fade-up" data-aos-duration="750">
 			<blockquote class="text-center">
-				<p class="text-xs font-semibold leading-loose text-gray-200 dark:text-slate-600">
+				<p class="text-xs font-serif font-semibold leading-loose text-gray-200 dark:text-slate-600">
 					"Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya, dan Dia menjadikan diantaramu rasa kasih dan sayang.
 					 Sungguh, pada yang demikian itu benar-benar terdapat tanda-tanda (kebesaran Allah) bagi kaum yang berpikir."
 				</p>
